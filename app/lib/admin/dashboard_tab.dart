97ad3be63bde77ae209e422d514bd9aa5ea58b37
jaumard <jimmy.aumard@gmail.com>
2021-11-29 12:04:00 +0100

    add admin events stats

--- app/lib/admin/dashboard_tab.dart
+++ app/lib/admin/dashboard_tab.dart
@@ -14,7 +14,7 @@ class Dashboard extends HookConsumerWidget {
       });
     }, const []);
 
-    if (store.dashboard == null) {
+    if (store.usersChart == null) {
       return const Loading();
     }
 
@@ -33,11 +33,28 @@ class Dashboard extends HookConsumerWidget {
                   child: ClipRect(
                     child: SfCircularChart(
                       title: ChartTitle(text: 'Utilisateurs'),
-                      series: store.dashboard!,
+                      series: store.usersChart!,
                       legend: Legend(
                         isVisible: true,
                         isResponsive: true,
                       ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(kNormalPadding),
+              child: Padding(
+                padding: const EdgeInsets.all(kNormalPadding),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 500),
+                  child: ClipRect(
+                    child: SfCartesianChart(
+                      primaryXAxis: CategoryAxis(),
+                      primaryYAxis: NumericAxis(minimum: 0, interval: 50),
+                      title: ChartTitle(text: 'Consultations'),
+                      series: store.eventsChart!,
                     ),
                   ),
                 ),
--- app/lib/admin/stores/dashboard_store.dart
+++ app/lib/admin/stores/dashboard_store.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heal_happy/common/network/api_provider.dart';
 import 'package:heal_happy_sdk/heal_happy_sdk.dart';
+import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 final adminDashboardStoreProvider = ChangeNotifierProvider((_) => AdminDashboardStore());
@@ -14,74 +15,129 @@ enum DashboardDataEnum {
   patientNonActivated,
 }
 
-class DashboardData {
+class UsersChartData {
   final DashboardDataEnum type;
   final int total;
 
-  DashboardData(this.type, this.total);
+  UsersChartData(this.type, this.total);
+}
+
+class EventsChartData {
+  final int month;
+  final int year;
+  final int total;
+
+  EventsChartData(this.month, this.year, this.total);
 }
 
 class AdminDashboardStore extends ChangeNotifier {
   final AdminApi _adminApi;
-  List<CircularSeries<DashboardData, String>>? dashboard;
+  List<CircularSeries<UsersChartData, String>>? usersChart;
+  List<ColumnSeries<EventsChartData, String>>? eventsChart;
   bool isLoading = false;
 
   AdminDashboardStore({AdminApi? adminApi}) : _adminApi = BackendApiProvider().api.getAdminApi();
+
+  void _manageEventsChart(Dashboard dashboard) async {
+    final eventsData = dashboard.events;
+    final _uiFormat = DateFormat('MMM');
+    final year = Date.now().year;
+    String? getCallback(EventsChartData row, _) {
+      final _date = DateTime(year, row.month, 1);
+      return _uiFormat.format(_date);
+    }
+
+    String? getCallbackLabel(EventsChartData row, _) {
+      return row.total.toString();
+    }
+    eventsChart = [
+      ColumnSeries(
+        dataSource: eventsData.reversed.map((p0) => EventsChartData(p0.month, p0.year, p0.total)).toList(),
+        dataLabelSettings: const DataLabelSettings(isVisible: true),
+        enableTooltip: true,
+        xValueMapper: getCallback,
+        yValueMapper: (EventsChartData data, _) => data.total,
+        // Set a label accessor to control the text of the bar label.
+        dataLabelMapper: getCallbackLabel,
+      )
+    ];
+  }
+
+  void _manageUsersChart(Dashboard dashboard) async {
+    final admins = dashboard.users.where((p0) => p0.type == UserTypeEnum.admin);
+    final data = <UsersChartData>[];
+    int total = 0;
+    for (final admin in admins) {
+      total += admin.total;
+    }
+    data.add(UsersChartData(DashboardDataEnum.admin, total));
+
+    final patients = dashboard.users.where((p0) => p0.type == UserTypeEnum.patient);
+
+    int totalActivated = 0;
+    int totalNonActivated = 0;
+    for (final patient in patients) {
+      if (patient.isActivated) {
+        totalActivated += patient.total;
+      } else {
+        totalNonActivated += patient.total;
+      }
+    }
+    data.add(UsersChartData(DashboardDataEnum.patientActivated, totalActivated));
+    data.add(UsersChartData(DashboardDataEnum.patientNonActivated, totalNonActivated));
+
+    final healers = dashboard.users.where((p0) => p0.type == UserTypeEnum.healer);
+
+    int totalVerified = 0;
+    int totalToVerify = 0;
+    for (final healer in healers) {
+      if (healer.isVerified) {
+        totalVerified += healer.total;
+      } else {
+        totalToVerify += healer.total;
+      }
+    }
+    data.add(UsersChartData(DashboardDataEnum.healerVerified, totalVerified));
+    data.add(UsersChartData(DashboardDataEnum.healerToVerify, totalToVerify));
+
+    String? getCallback(UsersChartData row, _) {
+      switch (row.type) {
+        case DashboardDataEnum.admin:
+          return '${row.total} Admin';
+        case DashboardDataEnum.healerVerified:
+          return '${row.total} Soignants vérifiés';
+        case DashboardDataEnum.healerToVerify:
+          return '${row.total} Soignants à vérifier';
+        case DashboardDataEnum.patientActivated:
+          return '${row.total} Patients actif';
+        case DashboardDataEnum.patientNonActivated:
+          return '${row.total} Patients inactif';
+      }
+    }
+
+    usersChart = [
+      PieSeries(
+        dataSource: data,
+        dataLabelSettings: const DataLabelSettings(isVisible: true),
+        enableTooltip: true,
+        explode: true,
+        explodeIndex: 4,
+        explodeOffset: '10%',
+        xValueMapper: getCallback,
+        yValueMapper: (UsersChartData data, _) => data.total,
+        // Set a label accessor to control the text of the arc label.
+        dataLabelMapper: getCallback,
+      )
+    ];
+  }
 
   Future<void> getDashboard() async {
     isLoading = true;
     notifyListeners();
     try {
       final results = await _adminApi.getDashboard();
-      final admins = results.data!.users.where((p0) => p0.type == UserTypeEnum.admin);
-      final data = <DashboardData>[];
-      int total = 0;
-      for (final admin in admins) {
-        total += admin.total;
-      }
-      data.add(DashboardData(DashboardDataEnum.admin, total));
-
-      final patients = results.data!.users.where((p0) => p0.type == UserTypeEnum.patient);
-
-      int totalActivated = 0;
-      int totalNonActivated = 0;
-      for (final patient in patients) {
-        if (patient.isActivated) {
-          totalActivated += patient.total;
-        } else {
-          totalNonActivated += patient.total;
-        }
-      }
-      data.add(DashboardData(DashboardDataEnum.patientActivated, totalActivated));
-      data.add(DashboardData(DashboardDataEnum.patientNonActivated, totalNonActivated));
-
-      final healers = results.data!.users.where((p0) => p0.type == UserTypeEnum.healer);
-
-      int totalVerified = 0;
-      int totalToVerify = 0;
-      for (final healer in healers) {
-        if (healer.isVerified) {
-          totalVerified += healer.total;
-        } else {
-          totalToVerify += healer.total;
-        }
-      }
-      data.add(DashboardData(DashboardDataEnum.healerVerified, totalVerified));
-      data.add(DashboardData(DashboardDataEnum.healerToVerify, totalToVerify));
-      dashboard = [
-        PieSeries(
-          dataSource: data,
-          dataLabelSettings: const DataLabelSettings(isVisible: true),
-          enableTooltip: true,
-          explode: true,
-          explodeIndex: 4,
-          explodeOffset: '10%',
-          xValueMapper: getCallback,
-          yValueMapper: (DashboardData data, _) => data.total,
-          // Set a label accessor to control the text of the arc label.
-          dataLabelMapper: getCallback,
-        )
-      ];
+      _manageUsersChart(results.data!);
+      _manageEventsChart(results.data!);
       isLoading = false;
       notifyListeners();
     } catch (error) {
@@ -90,18 +146,4 @@ class AdminDashboardStore extends ChangeNotifier {
     }
   }
 
-  String? getCallback(DashboardData row, _) {
-    switch (row.type) {
-      case DashboardDataEnum.admin:
-        return '${row.total} Admin';
-      case DashboardDataEnum.healerVerified:
-        return '${row.total} Soignants vérifiés';
-      case DashboardDataEnum.healerToVerify:
-        return '${row.total} Soignants à vérifier';
-      case DashboardDataEnum.patientActivated:
-        return '${row.total} Patients actif';
-      case DashboardDataEnum.patientNonActivated:
-        return '${row.total} Patients inactif';
-    }
-  }
 }
